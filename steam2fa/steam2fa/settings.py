74ff49c965c45b96f760666bc057159f98d73fff
Max <addeptoff@gmail.com>
2025-10-21 18:19:10 +0300

    Update settings.py

--- steam2fa/steam2fa/settings.py
+++ steam2fa/steam2fa/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["79.174.95.61", "steam2fa.ru", "www.steam2fa.ru", "localhost", "127.0.0.1"]
 #
 # CSRF_TRUSTED_ORIGINS = ["https://steam2fa.ru", "https://www.steam2fa.ru"]
 
